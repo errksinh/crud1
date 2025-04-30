4989d05e13c190faa70c46f18a8cc68d88d36158
errksinh <errksinh@gmail.com>
2025-04-30 16:29:06 +0530

    30-04-25
    create table,route,modole,controller
    16min.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/student',[StudentController::class,'index'])->name('student.index');
